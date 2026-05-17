df782c4caeda75fe6935f55c99b04a8b30cd98b1
Gaurav <GauravDhungana322@gmail.com>
2026-05-17 19:47:40 +0545

    routing done

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className="pt-24">
+      <h1 className="text-4xl">
+        this is about section
+      </h1>
+    </div>
+  )
+}
+
+export default About
--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -1,39 +1,47 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 
 const NavBar = () => {
   return (
-    <nav className="fixed top-0 left-0  bg-gray-100 w-full  shadow-md px-8 py-4 z-50">
+    <nav className="fixed top-0 left-0 bg-gray-100 w-full shadow-md px-8 py-4 z-50">
       <div className="flex items-center justify-between">
 
         {/* Logo */}
         <div className="flex items-center gap-3">
-          
-          <h1 className="text-2xl font-bold text-gray-800 font-serif">
-            Nepal Trek
-          </h1>
+          <Link to="/">
+            <h1 className="text-2xl font-bold text-gray-800 font-serif hover:cursor-pointer">
+              Nepal Trek
+            </h1>
+          </Link>
         </div>
 
         {/* Menu */}
         <ul className="flex gap-10 text-lg font-semibold text-gray-700">
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">
-            HOME
-          </li>
+          <Link to="/">
+            <li className="hover:text-blue-600 cursor-pointer transition duration-300">
+              HOME
+            </li>
+          </Link>
 
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">
-            ABOUT
-          </li>
+          <Link to="/about">
+            <li className="hover:text-blue-600 cursor-pointer transition duration-300">
+              ABOUT
+            </li>
+          </Link>
 
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">
-            TREKKING IN NEPAL
-          </li>
+          <Link to="/contact">
+            <li className="hover:text-blue-600 cursor-pointer transition duration-300">
+              CONTACT
+            </li>
+          </Link>
 
-          <li className="hover:text-blue-600 cursor-pointer transition duration-300">
-            PHOTO GALLERY
-          </li>
+          <Link to="/photoGallery">
+            <li className="hover:text-blue-600 cursor-pointer transition duration-300">
+              PHOTO GALLERY
+            </li>
+          </Link>
         </ul>
       </div>
-     
     </nav>
   )
 }
