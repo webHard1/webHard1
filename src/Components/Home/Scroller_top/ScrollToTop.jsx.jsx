b73e6c19908bad28a2b7b77d46464cb4bdba590d
Gaurav <GauravDhungana322@gmail.com>
2026-05-21 15:09:41 +0545

    scroll to top done

--- src/Components/Home/Scroller_top/ScrollToTop.jsx.jsx
+++ src/Components/Home/Scroller_top/ScrollToTop.jsx.jsx
@@ -4,7 +4,7 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname } = useLocation(); 
 
   useEffect(() => {
     window.scrollTo(0, 0);
@@ -12,5 +12,5 @@ const ScrollToTop = () => {
 
   return null;
 };
-
+ 
 export default ScrollToTop;
