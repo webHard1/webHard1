c4a935d2952ca38d0634febc268a63c468ab23ca
Gaurav <GauravDhungana322@gmail.com>
2026-05-22 13:17:32 +0545

    navbar responsivness fix

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const [mobileAboutOpen, setMobileAboutOpen] = useState(false)
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-100 shadow-md h-[60px] px-4 sm:px-6 md:px-8 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-gray-100 shadow-md h-[80px] px-4 sm:px-6 md:px-8 z-50">
 
       <div className="flex items-center justify-between h-full">
 
--- src/Components/Scroller_top/ScrollToTop.jsx.jsx
+++ src/Components/Scroller_top/ScrollToTop.jsx.jsx
@@ -4,13 +4,13 @@ import { useEffect } from "react";
 import { useLocation } from "react-router-dom";
 
 const ScrollToTop = () => {
-  const { pathname } = useLocation(); 
+  const { pathname } = useLocation();
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, [pathname]);
 
-  return null;
+
 };
- 
+
 export default ScrollToTop;
