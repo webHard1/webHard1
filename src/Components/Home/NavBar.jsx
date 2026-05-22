9584e60b455564d78571ab895f2725075cb4cc81
Gaurav <GauravDhungana322@gmail.com>
2026-05-22 13:24:19 +0545

    change in GAP

--- src/Components/Home/NavBar.jsx
+++ src/Components/Home/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
   const [mobileAboutOpen, setMobileAboutOpen] = useState(false)
 
   return (
-    <nav className="fixed top-0 left-0 w-full bg-gray-100 shadow-md h-[80px] px-4 sm:px-6 md:px-8 z-50">
+    <nav className="fixed top-0 left-0 w-full bg-gray-100 shadow-md h-[60px] px-4 sm:px-6 md:px-8 z-50">
 
       <div className="flex items-center justify-between h-full">
 
@@ -24,7 +24,7 @@ const NavBar = () => {
         </Link>
 
         {/* Desktop Menu */}
-        <ul className="hidden md:flex items-center gap-10 font-semibold text-gray-700">
+        <ul className="hidden md:flex items-center gap-8 font-semibold text-gray-700">
 
           <Link to="/">
             <li className="hover:text-red-900 cursor-pointer transition duration-300">
