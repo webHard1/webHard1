383e6ce918415844af776ac3d6366bff0fe15cf0
Gaurav <GauravDhungana322@gmail.com>
2026-05-25 12:14:56 +0545

    comt

--- src/Components/Home/Footer.jsx
+++ src/Components/Home/Footer.jsx
@@ -151,7 +151,7 @@ const Footer = () => {
       </div>
 
       <div className="w-full  border-t h-10 flex items-center justify-center text-gray-300 font-bold text-center px-4">
-        © {new Date().getFullYear()} Miteri Treks & Expedition.
+        © {new Date().getFullYear()} 2026 Miteri Treks Pvt. Ltd. All rights reserved.
       </div>
 
     </footer>
